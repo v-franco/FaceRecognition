23ca53c0a04573139e96a8060fe0e8dc0746e519
Victor Franco <vfranco.itc@gmail.com>
2023-04-27 19:03:15 -0600

    Add Design Matrix

---
+++ matrixRecognition.py
@@ -0,0 +1,57 @@
+from IPython.lib.display import FileLinks
+import pandas
+import numpy
+import cv2
+import face_recognition
+import glob
+
+# Esto solo se hace una vez en el BackEnd
+
+def DesignMatrix(Options):
+  Files = glob.glob("photos/TC3002B_Faces/*/*.jpg")
+  OutFile = Options.OutFile # Nombre del archivo de salida en CSV
+  X = [] # Matriz de diseño vacia
+  L = [] # Etiquetas de a quien pertenece la fotografia
+  for File in Files:
+    # Leer la imagen de la cara y guardala en la matrix X como un vector
+    #x = numpy.random.rand(512);
+    x = Image2Vector(File)
+    X.append(x)
+    L.append(File)
+    print(File)
+
+  DF = pandas.DataFrame(X)
+  DF.insert(0,"File",L)
+  DF.to_csv(Options.OutFile, index = False)
+
+def Image2Vector(File):
+  I = cv2.imread(File) # Leer la imagen de la foto
+  AR = 480 / I.shape[1] # Aspect Ratio
+  width = int(I.shape[1] * AR)
+  height = int(I.shape[0] * AR)
+  # Reescalamiento
+  I = cv2.resize(I, (width,height), interpolation = cv2.INTER_AREA)
+  cv2.imwrite("temp.jpg", I)
+  # Guardar archivo temporal de la imagen guardada
+  FID = face_recognition.load_image_file("temp.jpg") # carga de imagen reescalada
+  Locations = face_recognition.face_locations(FID)
+  FaceVectors = face_recognition.face_encodings(FID, Locations)
+  x = FaceVectors[0]
+  return x
+
+def Similiarity(a,b):
+  # metrica de similitud
+  # depende de la naturaleza del manifold
+  # puede ser que se haga pequeña (caso L2) o se haga grande (caso Producto interno)
+  # Tarea: tener varias metricas en esta funcion para seleccionar una
+  return numpy.linalg.norm(a-b)
+
+class Opt:
+  pass
+
+Options = Opt
+
+Options.SPath = "photos/TC3002B_Faces/"
+Options.OutFile = "Faces.csv"
+
+DesignMatrix(Options)
